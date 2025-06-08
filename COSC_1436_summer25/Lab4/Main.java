423c24994feb46c2b43d7b8235fc5470bc2fa996
Plebarino <113859478+Plebarino@users.noreply.github.com>
2025-06-08 08:06:03 +0000

    Lab4 Repo commit 1.0

---
+++ COSC_1436_summer25/Lab4/Main.java
@@ -0,0 +1,19 @@
+// Creates a class called "Main".
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Creates a Book object
+        Book myBook = new Book("Made an Example", 19.99);
+        
+        // Creates an Author object.
+        Author bookAuthor = new Author("Nolan Hollingsworth", "nolan@example.com", "American");
+        
+        // Links author to book and print book title.
+        myBook.setWriter(bookAuthor);
+        System.out.println(myBook);
+
+        // Links book to author and print book author.
+        bookAuthor.setBook(myBook);
+    }
+}
