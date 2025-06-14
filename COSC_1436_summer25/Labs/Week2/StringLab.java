20ceb652b4bf6f46efabb8b91d82b0e0370a5d3c
Plebarino <113859478+Plebarino@users.noreply.github.com>
2025-06-14 00:53:22 +0000

    changes

--- COSC_1436_summer25/Labs/Week2/StringLab.java
+++ COSC_1436_summer25/Labs/Week2/StringLab.java
@@ -1,12 +1,37 @@
-package Labs.Week2;
+// // what i have to use
+// length():
+// charAt():
+// substring():
+// toUpperCase():
+// indexOf():
+// contains():
+// replace():
+// split():
+// trim():
+// equals()
 
-import java.util.Scanner; // imports the scanner utility
 
-public class StringLab // This creates a new class named StringLab.
+package Labs.Week2; // location of current lab
+
+// IM NOT SURE I NEED THIS
+// import java.util.Scanner; // imports the scanner utility
+
+public class StringLab // creates a new class named StringLab.
 {
-    public static void main(String[] args) // This defines the main method and is the beginning of the program.
-
+    public static void main(String[] args) // defines the main method and is the beginning of the program.
     {
+        String homework = "Welcome to the Java String Lab!" // assigns the string homework
+
+        int len = homework.length(); // grabs the length of the string
+        System.out.print(len); // prints thelength of the string
+
+        char letter = homework.charAt(7); // grabs letter at index 7
+        System.out.print(letter); // prints letter at index 7
+
+        System.out.print(homework.substring(15,18)); // prints "Java"
+
+
+
 
     }
 
